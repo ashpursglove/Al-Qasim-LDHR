39364c87684606ae7b4ee057ca2a65bc1b9a2fd9
ashpursglove <a.b.pursglove@gmail.com>
2020-11-07 02:50:16 +0000

    Addition of pumps and switches

--- ldhr/ldhr.py
+++ ldhr/ldhr.py
@@ -18,6 +18,46 @@ run = 0
 
 
 
+
+# GPIO:
+# 4  - Inside greenhouse DHT22
+# 17 - Outside greenhouse DHT22
+# 27 - Other greenhouse DHT22
+# 
+# 
+# 5  - Pump 1 inlet cooling, Pump 2 outlet cooling, fans
+# 6  - Pump 3 inlet tank out
+# 13 - Pump 4 outlet tank out
+# 19 - Pump 5 staging tank out
+# 
+# 26 - Manual Control Over-Ride
+# 18 - Manual Fan
+# 23 - Manual Cooling pumps (1&2)
+# 24 - Manual inlet tank out (Pump 3)
+# 25 - Manual Outlet Tank out (Pump 4)
+# 12 - Manual Staging Tank out (Pump 5)
+# 16 - inlet float switch 
+# 20 - outlet float switch
+# 21 - Staging float switch
+
+GPIO.setmode(GPIO.BCM)
+
+GPIO.setup(5, GPIO.OUT)
+GPIO.setup(6, GPIO.OUT)
+GPIO.setup(13, GPIO.OUT)
+GPIO.setup(19, GPIO.OUT)
+
+GPIO.setup(26, GPIO.IN)
+GPIO.setup(18, GPIO.IN)
+GPIO.setup(23, GPIO.IN)
+GPIO.setup(24, GPIO.IN)
+GPIO.setup(25, GPIO.IN)
+GPIO.setup(12, GPIO.IN)
+GPIO.setup(16, GPIO.IN)
+GPIO.setup(20, GPIO.IN)
+GPIO.setup(21, GPIO.IN)
+
+
 #function to get all T&H data
 def get_sensor_data():
     #get data from inside sensor
@@ -37,9 +77,11 @@ def get_sensor_data():
 
     data_arr[4] = other_arr[0] #other temp
     data_arr[5] = other_arr[1]  #other hum
-    GPIO.cleanup() # cleanup all GPIOs and close channels!!!
+    #GPIO.cleanup() # cleanup all GPIOs and close channels!!!
     
     print("\n"*100)
+    print("Auto Program Running")
+    print("")
     print("Log Number: %d" %(log_number))
     print("")
     print("Inside Temperature: %.1fC" %(data_arr[0]))
@@ -69,23 +111,97 @@ if run == 0:
         time.sleep(2)
         run = 1
         r = "x"
-        
+      
+      
+      
+      
+      
 while run == 1:
+    
     get_sensor_data()
+    
+    
+    manual_or = (GPIO.input(26))
+    inlet_empty = (GPIO.input(16))
+    outlet_empty = (GPIO.input(20))
+    stage_empty = (GPIO.input(21))
+    
+    
+    
     log_number += 1
     
     
-    
-    
+    # see if manual switch has been flipped
+    if GPIO.input(26):
+        run = 2
+        
     
   #if the inside is less than 5% different from outside    1 = inside hum, 3 = outside hum
     if data_arr[1] <= data_arr[3]+5:
         tank_switch = True
         
-        
+  # if temperature is above setpoint turn on cooling
     if data_arr[0] >= temp_setpoint:
         cooling = True
+        GPIO.output(5, GPIO.LOW)
+    else:
+        cooling = False
+        GPIO.output(5, GPIO.HIGH)
         
+    
+    
+    
+    
+    
+    
+    
+    
+    #Manual mode
+    
+    
+    while run == 2:
+        manual_or = (GPIO.input(26))
+        manual_cool = (GPIO.input(23))
+        manual_inletout = (GPIO.input(24))
+        manual_outletout = (GPIO.input(25))
+        manual_stout = (GPIO.input(12))
+        
+        
+        
+        if manual_or == 0:
+            
+            run = 1
+        
+        
+        
+        if manual_cool:
+            GPIO.output(5, GPIO.LOW)
+        else:
+            GPIO.output(5, GPIO.HIGH)
+            
+            
+        if manual_inletout:
+            GPIO.output(6, GPIO.LOW)
+        else:
+            GPIO.output(6, GPIO.HIGH)
+            
+            
+        if manual_outletout:
+            GPIO.output(13, GPIO.LOW)
+        else:
+            GPIO.output(13, GPIO.HIGH)
+            
+            
+        if manual_stout:
+            GPIO.output(19, GPIO.LOW)
+        else:
+            GPIO.output(19, GPIO.HIGH)
+            
+        print("manual mode")
+
+
+ 
+
         
         
     
